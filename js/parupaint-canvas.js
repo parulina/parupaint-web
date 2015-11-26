064a2935898c9a8cdffec71474dbee731b91b3fe
paru <paru@sqnya.se>
2015-11-26 09:34:07 +0100

    hexnormalize

--- js/parupaint-canvas.js
+++ js/parupaint-canvas.js
@@ -2,10 +2,12 @@ function hexnormalize(hex){
 	if(!hex) return hex;
 
 	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-	hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-		return r + r + g + g + b + b;
-	});
+	if(hex.length <= 4){
+		var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+		hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+			return r + r + g + g + b + b;
+		});
+	}
 	if(hex[0] != '#') hex = '#' + hex;
 	return hex;
 }
